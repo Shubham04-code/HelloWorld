081941eba3b5c9af5d771c395ccce02358ca3cea
Shubham Kumar Singh <shubham26262727@gmail.com>
2022-08-16 12:59:17 +0530

    Add files via upload

---
+++ helloworld.c
@@ -0,0 +1,11 @@
+/*Author: Shubham Kumar Singh
+Purpose of program: C program to print the Hello World */
+
+#include <stdio.h>
+int main()
+{
+    printf("Hello World\n");
+
+    return 0;
+    
+}
